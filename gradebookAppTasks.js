0651f9c5c1bc87d58bb84a20fe8844174e6a75df
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-07-30 17:42:10 +0300

    Step 2 - getGrade function

--- gradebookAppTasks.js
+++ gradebookAppTasks.js
@@ -10,3 +10,35 @@ function getAverage(scores) {
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]))
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]))
+
+//another solution
+function getAverage(scores) {
+  let sum = 0
+
+  for (const score of scores) {
+    sum += score
+  }
+
+  return sum / scores.length
+}
+
+//step 2
+function getGrade(score) {
+  if (score === 100) {
+    return 'A++'
+  } else if (score <= 59) {
+    return 'F'
+  } else if (score <= 69) {
+    return 'D'
+  } else if (score <= 79) {
+    return 'C'
+  } else if (score <= 89) {
+    return 'B'
+  } else if (score <= 99) {
+    return 'A'
+  }
+}
+
+console.log(getGrade(96))
+console.log(getGrade(82))
+console.log(getGrade(56))
