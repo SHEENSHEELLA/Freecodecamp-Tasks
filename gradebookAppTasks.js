4f27011b2fc547dbacc7048b366a76c3fa161fee
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-07-30 18:22:21 +0300

    Step 4 - studentMsg function

--- gradebookAppTasks.js
+++ gradebookAppTasks.js
@@ -60,3 +60,16 @@ console.log(hasPassingGrade(87))
 function hasPassingGrade(score) {
   return getGrade(score) !== 'F'
 }
+
+//step 4
+function studentMsg(totalScores, studentScore) {
+  const average = getAverage(totalScores)
+  const grade = getGrade(studentScore)
+
+  if (hasPassingGrade(studentScore)) {
+    return `Class average: ${average}. Your grade: ${grade}. You passed the course.`
+  } else {
+    return `Class average: ${average}. Your grade: ${grade}. You failed the course.`
+  }
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37))
