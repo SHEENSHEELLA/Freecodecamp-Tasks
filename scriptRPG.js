b42eaaecc55dcc7281dbafc35ab857ae7ade4a8b
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-01 10:50:19 +0300

    RPG - step 66

--- scriptRPG.js
+++ scriptRPG.js
@@ -47,12 +47,10 @@ button1.onclick = goStore
 button2.onclick = goCave
 button3.onclick = fightDragon
 
-function update(location) {}
-
-function goTown() {
-  button1.innerText = 'Go to store'
-  button2.innerText = 'Go to cave'
-  button3.innerText = 'Fight dragon'
+function update(location) {
+  button1.innerText = location['button text'][0]
+  button2.innerText = location['button text'][1]
+  button3.innerText = location['button text'][2]
   button1.onclick = goStore
   button2.onclick = goCave
   button3.onclick = fightDragon
@@ -60,15 +58,12 @@ function goTown() {
     'You are in the town square. You see a sign that says "Store".'
 }
 
+function goTown() {
+  update(locations[0])
+}
+
 function goStore() {
-  console.log('Going to store.')
-  button1.innerText = 'Buy 10 health (10 gold)'
-  button2.innerText = 'Buy weapon (30 gold)'
-  button3.innerText = 'Go to town square'
-  button1.onclick = buyHealth
-  button2.onclick = buyWeapon
-  button3.onclick = goTown
-  text.innerText = 'You enter the store.'
+  update(locations[1])
 }
 
 function goCave() {
