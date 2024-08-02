ddc5b25643fb15ea27cb1c119f49653a8e6a336c
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-02 13:06:51 +0300

    RPG - step 104

--- scriptRPG.js
+++ scriptRPG.js
@@ -109,6 +109,17 @@ function buyWeapon() {
     } else {
       text.innerText = 'You do not have enough gold to buy a weapon.'
     }
+  } else {
+    text.innerText = 'You already have the most powerful weapon!'
+    button2.innerText = 'Sell weapon for 15 gold'
+    button2.onclick = sellWeapon
+  }
+}
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15
+    goldText.innerText = gold
   }
 }
 
