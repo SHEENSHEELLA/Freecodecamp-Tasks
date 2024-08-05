3cbf55629d6a446ec47d9d96ecaf20b7c7dfd1ec
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-05 13:53:24 +0300

    RPG - step 125

--- scriptRPG.js
+++ scriptRPG.js
@@ -169,6 +169,10 @@ function attack() {
   text.innerText +=
     ' You attack it with your ' + weapons[currentWeaponIndex].name + '.'
   health -= monsters[fighting].level
+  monsterHealth -=
+    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1
+  healthText.innerText = health
+  monsterHealthText.innerText = monsterHealth
 }
 
 function dodge() {}
