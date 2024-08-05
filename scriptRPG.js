4693284d0e2411c69a701c83f96009fc54c70aae
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-05 15:45:38 +0300

    RPG - step 131

--- scriptRPG.js
+++ scriptRPG.js
@@ -173,6 +173,20 @@ function attack() {
     weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1
   healthText.innerText = health
   monsterHealthText.innerText = monsterHealth
+  if (health <= 0) {
+    lose()
+  } else if (monsterHealth <= 0) {
+    defeatMonster()
+  }
 }
 
-function dodge() {}
+function dodge() {
+  text.innerText = 'You dodge the attack from the ' + monsters[fighting].name
+}
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7)
+  xp += monsters[fighting].level
+}
+
+function lose() {}
