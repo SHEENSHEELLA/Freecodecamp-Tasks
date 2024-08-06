6f5d8a4fd9b97bf5b0d7cef51fcd9e7ca9ba034e
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-06 16:28:50 +0300

    RPG - step 151

--- scriptRPG.js
+++ scriptRPG.js
@@ -192,8 +192,12 @@ function attack() {
   text.innerText +=
     ' You attack it with your ' + weapons[currentWeaponIndex].name + '.'
   health -= getMonsterAttackValue(monsters[fighting].level)
-  monsterHealth -=
-    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1
+  if (isMonsterHit()) {
+    monsterHealth -=
+      weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1
+  } else {
+    text.innerText += ' You miss.'
+  }
   healthText.innerText = health
   monsterHealthText.innerText = monsterHealth
   if (health <= 0) {
