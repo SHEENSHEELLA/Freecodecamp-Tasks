76f151393f690393b6e21208a980121a4d19e5ab
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-12 15:43:18 +0300

    CCount - step 60

--- 05-calorieCounter/scriptCCount.js
+++ 05-calorieCounter/scriptCCount.js
@@ -31,4 +31,11 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString)
 }
 
+function getCaloriesFromInputs(list) {
+  let calories = 0
+  for (const item of list) {
+    const currVal = cleanInputString(item.value)
+  }
+}
+
 addEntryButton.addEventListener('click', addEntry)
