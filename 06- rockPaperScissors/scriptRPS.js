372e0e50e10144a5cd6f40909b97fe5cf1e3ce0b
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-15 17:13:20 +0300

    RPS - step 4

--- 06- rockPaperScissors/scriptRPS.js
+++ 06- rockPaperScissors/scriptRPS.js
@@ -27,3 +27,15 @@ function getRoundResults(userOption) {
     return `Computer wins! ${computerResult} beats ${userOption}`
   }
 }
+
+const playerScoreSpanElement = document.getElementById('player-score')
+const computerScoreSpanElement = document.getElementById('computer-score')
+const roundResultsMsg = document.getElementById('results-msg')
+
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption)
+  playerScoreSpanElement.innerText = playerScore
+  computerScoreSpanElement.innerText = computerScore
+}
+
+showResults('Rock')
