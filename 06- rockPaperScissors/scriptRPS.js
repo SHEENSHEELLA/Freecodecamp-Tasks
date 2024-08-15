965436a17fdf3b573d29c5d00c728ae19317958e
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-15 16:27:55 +0300

    RPS - step 2

--- 06- rockPaperScissors/scriptRPS.js
+++ 06- rockPaperScissors/scriptRPS.js
@@ -1,6 +1,16 @@
 function getRandomComputerResult() {
-  const options = ["Rock", "Paper", "Scissors"];
-const randomIndex = Math.floor(Math.random() * options.length);
-return options[randomIndex];
+  const options = ['Rock', 'Paper', 'Scissors']
+  const randomIndex = Math.floor(Math.random() * options.length)
+  return options[randomIndex]
 }
-console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+  if (
+    (player === 'Rock' && computer === 'Scissors') ||
+    (player === 'Scissors' && computer === 'Paper') ||
+    (player === 'Paper' && computer === 'Rock')
+  ) {
+    return true
+  }
+  return false
+}
