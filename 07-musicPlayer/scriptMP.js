bad02ac78b0a236bce601f58dbb248908f8884aa
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-16 15:09:25 +0300

    MP - step 15

--- 07-musicPlayer/scriptMP.js
+++ 07-musicPlayer/scriptMP.js
@@ -86,3 +86,13 @@ let userData = {
   currentSong: null,
   songCurrentTime: 0,
 }
+
+const printGreeting = () => {
+  console.log('Hello there!')
+}
+
+printGreeting('freeCodeCamp')
+
+const addTwoNumbers = (num1, num2) => num1 + num2
+
+console.log(addTwoNumbers(3, 4))
