3d46471e0f2466df242698194247ca11b8d2fd9d
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-22 15:54:08 +0300

    MP - step 69

--- 07-musicPlayer/scriptMP.js
+++ 07-musicPlayer/scriptMP.js
@@ -99,6 +99,7 @@ const playSong = (id) => {
   }
   userData.currentSong = song
   playButton.classList.add('playing')
+  highlightCurrentSong()
   audio.play()
 }
 
@@ -128,11 +129,22 @@ const playPreviousSong = () => {
   }
 }
 
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+};
+
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll('.playlist-song')
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   )
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute('aria-current')
+  })
+  if (songToHighlight) {
+    songToHighlight.setAttribute('aria-current', 'true')
+  }
 }
 
 const renderSongs = (array) => {
