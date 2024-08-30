66ea22c3e74690d51140a0cf5b63727f552aff7c
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-30 14:22:41 +0300

    DateFormatter - step 29 finish project

--- 09-dateFormatter/scriptDFor.js
+++ 09-dateFormatter/scriptDFor.js
@@ -1,9 +1,6 @@
 const currentDateParagraph = document.getElementById('current-date')
 const dateOptionsSelectElement = document.getElementById('date-options')
 
-console.log(dateOptionsSelectElement)
-console.log(dateOptionsSelectElement.value)
-
 const date = new Date()
 const day = date.getDate()
 const month = date.getMonth() + 1
@@ -14,11 +11,21 @@ const formattedDate = `${day}-${month}-${year}`
 
 currentDateParagraph.textContent = formattedDate
 
-dateOptionsSelectElement.addEventListener('change', () => {})
+dateOptionsSelectElement.addEventListener('change', () => {
+  switch (dateOptionsSelectElement.value) {
+    case 'yyyy-mm-dd':
+      currentDateParagraph.textContent = formattedDate
+        .split('-')
+        .reverse()
+        .join('-')
+      break
+    case 'mm-dd-yyyy-h-mm':
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`
+      break
+    default:
+      currentDateParagraph.textContent = formattedDate
+  }
+})
 
-switch (dateOptionsSelectElement.value) {
-  case "yyyy-mm-dd":
-    currentDateParagraph.textContent = formattedDate
-      .split("-")
-      .reverse().join("-")
-}
+console.log(dateOptionsSelectElement)
+console.log(dateOptionsSelectElement.value)
