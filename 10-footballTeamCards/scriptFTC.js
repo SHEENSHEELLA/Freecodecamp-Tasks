b3786f198a96be6c5b631290810b5ceb3501bd45
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-31 11:49:50 +0300

    FTC - step 5

--- 10-footballTeamCards/scriptFTC.js
+++ 10-footballTeamCards/scriptFTC.js
@@ -1,2 +1,13 @@
 const teamName = document.getElementById('team')
 console.log(teamName)
+
+const typeOfSport = document.getElementById('sport')
+const worldCupYear = document.getElementById('year')
+const headCoach = document.getElementById('head-coach')
+const playerCards = document.getElementById('player-cards')
+const playersDropdownList = document.getElementById('players')
+
+const myFavoriteFootballTeam = {}
+
+console.log(playerCards)
+console.log(playersDropdownList)
