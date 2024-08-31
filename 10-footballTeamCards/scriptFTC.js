19874461e7d11b5592af6f824f3684e03e37db3e
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-08-31 13:05:07 +0300

    FTC - step 20

--- 10-footballTeamCards/scriptFTC.js
+++ 10-footballTeamCards/scriptFTC.js
@@ -173,4 +173,7 @@ const myFavoriteFootballTeam = {
 }
 
 Object.freeze(myFavoriteFootballTeam)
-const sport = myFavoriteFootballTeam.sport
+
+const { sport, team, year, players } = myFavoriteFootballTeam
+const { coachName } = myFavoriteFootballTeam.headCoach
+typeOfSport.textContent = sport
